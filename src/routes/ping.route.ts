d431a58f0259902ced1ddd1326d20b93f06a0f45
flavian.eng <engevinflavian@gmail.com>
2021-12-12 22:26:55 +0100

    🔥 Remove comment

--- src/routes/ping.route.ts
+++ src/routes/ping.route.ts
@@ -3,7 +3,6 @@ import * as pingController from "../controllers/ping.controller.ts";
 
 const router = new Router();
 
-// TASK Add some test
 router.all("/", pingController.sendPong);
 
 export default router;
