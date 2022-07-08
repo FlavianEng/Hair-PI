f57ab6e7bb622fe2d0069158341cc4b084397f5e
Flavian Engevin <flavian.engevin@ynov.com>
2022-07-08 15:36:11 +0200

    Updates engine and adds tests

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,6 +1,12 @@
 import { EOL } from "./models/engine.model.ts";
 
 export class Engine {
+  eol: EOL;
+
+  constructor(eol: EOL) {
+    this.eol = eol;
+  }
+
   createWordPhoneticObject(data: string[]): Record<string, string> {
     if (data.length % 2) {
       throw new Error("Length must be even");
@@ -23,9 +29,9 @@ export class Engine {
     return wordPhoneticObject;
   }
 
-  createWordListFromPlainText(path: string, eol: EOL): string[] {
+  createWordListFromPlainText(path: string): string[] {
     const fileAsString: string = Deno.readTextFileSync(path);
-    return fileAsString.split(eol);
+    return fileAsString.split(this.eol);
   }
 
   async createJsonFile(
@@ -36,4 +42,126 @@ export class Engine {
     await Deno.writeTextFile(path, JSON.stringify(data));
     console.info(`💾  .json file created to path: ${path}`);
   }
+
+  async createTextFile(
+    path: string,
+    data: string,
+  ): Promise<void> {
+    await Deno.open(path, { create: true, write: true });
+    await Deno.writeTextFile(path, data);
+    console.info(`📁 .txt File created to ${path}`);
+  }
+
+  readJsonFile(path: string): Record<string, string> {
+    const decoder = new TextDecoder("utf-8");
+    const data = Deno.readFileSync(path);
+    const decodedData = decoder.decode(data);
+    return JSON.parse(decodedData);
+  }
+
+  createUniqueWordList(arrayOfStrings: string[]): string[] {
+    return [...new Set(arrayOfStrings)];
+  }
+
+  createStringifiedWordList(array: string[]): string {
+    return array.join(this.eol);
+  }
+
+  filterObjectByPhonetics(
+    arrayOfPhonetics: Array<string>,
+    dataPairObject: Record<string, string>,
+  ) {
+    let filteredObject: Record<string, string> = {};
+    const dataPair = Object.entries(dataPairObject);
+
+    arrayOfPhonetics.forEach((phonetic: string) => {
+      const filtered = dataPair.filter(([_key, value]: [string, string]) =>
+        value.includes(phonetic) && phonetic !== value
+      );
+      const results = Object.fromEntries(filtered);
+
+      filteredObject = { ...filteredObject, ...results };
+    });
+
+    return filteredObject;
+  }
+
+  getVerbRegex(arrayOfTerminations: string[]) {
+    const setOfTerminations = [...new Set(arrayOfTerminations)];
+    let regex = "";
+
+    setOfTerminations.forEach((termination, index) => {
+      let str = `(${termination}$)|`;
+
+      if (index === setOfTerminations.length - 1) {
+        str = `(${termination}$)`;
+      }
+
+      regex += str;
+    });
+
+    return new RegExp(regex);
+  }
+
+  filterArrayByVerbTerminations(
+    arrayOfVerbTerminations: string[],
+    arrayOfWords: string[],
+  ): string[] {
+    const verbRegex: RegExp = this.getVerbRegex(arrayOfVerbTerminations);
+
+    const filteredArray = arrayOfWords.filter((word) => {
+      const isMatching = word.match(verbRegex) ? false : true;
+      return isMatching;
+    });
+
+    return filteredArray;
+  }
+
+  filterObjectByVerbTerminations(
+    arrayOfVerbTerminations: string[],
+    dataPairObject: Record<string, string>,
+  ): Record<string, string> {
+    const verbRegex: RegExp = this.getVerbRegex(arrayOfVerbTerminations);
+    const dataPair = Object.entries(dataPairObject);
+
+    const filtered = dataPair.filter(([key]) => {
+      const isMatching = key.match(verbRegex) ? false : true;
+      return isMatching;
+    });
+
+    const filteredObject = Object.fromEntries(filtered);
+
+    return { ...filteredObject };
+  }
+
+  findNumberOfOccurrenceInWord = (occurrence: string, word: string): number => {
+    return word.split(occurrence).length - 1;
+  };
+
+  removeDashQuotation = (word: string): string => {
+    const indexNum = word.indexOf("-");
+    return `${word.slice(0, indexNum)}${word.slice(indexNum + 1)}`;
+  };
+
+  capitalize = (wordParts: string[]): string => {
+    return `${wordParts[0].slice(0, 1).toUpperCase()}${wordParts[0].slice(1)}`;
+  };
+
+  replaceWordSegment = (
+    wordParts: string[],
+    replacingWord: string,
+    hasOneLetterAtEnd: boolean,
+  ): string => {
+    const hasAnEmptyPart = wordParts.some((part) => part === "");
+
+    if (hasAnEmptyPart) {
+      const emptyPart = wordParts.indexOf("");
+      wordParts[emptyPart] = replacingWord;
+      return wordParts.join("'");
+    }
+
+    return `${wordParts[0]}'${replacingWord}${hasOneLetterAtEnd ? "" : "'"}${
+      hasOneLetterAtEnd ? "" : wordParts[1]
+    }`;
+  };
 }
--- src/tests/engine/engine.test.ts
+++ src/tests/engine/engine.test.ts
@@ -9,19 +9,20 @@ import {
   describe,
   it,
 } from "https://deno.land/std@0.142.0/testing/bdd.ts";
+import { EOL } from "../../engine/models/engine.model.ts";
 
 describe("Engine", () => {
   let engine: Engine;
 
   beforeEach(() => {
-    engine = new Engine();
+    engine = new Engine(EOL.LF);
   });
 
-  it("It should create the engine instance", () => {
+  it("Should create the engine instance", () => {
     assertExists(engine);
   });
 
-  it("It should throw and error because the length is not even", () => {
+  it("Should throw and error because the length is not even", () => {
     const array = ["1", "2", "3"];
     assertThrows(
       () => engine.createWordPhoneticObject(array),
@@ -30,10 +31,121 @@ describe("Engine", () => {
     );
   });
 
-  it("It should create a word-phonetic object", () => {
+  it("Should create a word-phonetic object", () => {
     const array = ["1", "2", "3", "4"];
     const object = engine.createWordPhoneticObject(array);
 
     assertEquals(object, { 1: "2", 3: "4" });
+  });
+
+  it("Should create a unique list of words", () => {
+    const array = ["apple", "orange", "orange"];
+    const uniqueWordList = engine.createUniqueWordList(array);
+
+    assertEquals(uniqueWordList, ["apple", "orange"]);
+  });
+
+  it("Should stringify a word list", () => {
+    const array = ["apple", "orange"];
+    const stringifiedWordList = engine.createStringifiedWordList(array);
+
+    assertEquals(
+      stringifiedWordList,
+      `apple
+orange`,
+    );
+  });
+
+  it("Should filter object by phonetic", () => {
+    const arrayOfPhonetic = ["ɛr", "ɛɾ"];
+    const dataPairObject = {
+      "hits": "i",
+      "amerrir": "amɛriɾ",
+      "hélicoptère": "elikɔptɛɾə",
+    };
+
+    const filteredObject = engine.filterObjectByPhonetics(
+      arrayOfPhonetic,
+      dataPairObject,
+    );
+
+    assertEquals(filteredObject, {
+      amerrir: "amɛriɾ",
+      "hélicoptère": "elikɔptɛɾə",
+    });
+  });
+
+  it("Should get a regex for verbs", () => {
+    const arrayOfTerminations = ["ent", "ait"];
+    const verbRegex = engine.getVerbRegex(arrayOfTerminations);
+
+    assertEquals(verbRegex, /(ent$)|(ait$)/);
+  });
+
+  it("Should filter array by verbTerminations", () => {
+    const arrayOfTerminations = ["ent", "ait"];
+    const arrayOfWords = ["Voler", "Volent", "Volait"];
+    const filteredArray = engine.filterArrayByVerbTerminations(
+      arrayOfTerminations,
+      arrayOfWords,
+    );
+
+    assertEquals(filteredArray, ["Voler"]);
+  });
+
+  it("Should filter dataPairObject by verbTerminations", () => {
+    const arrayOfTerminations = ["ent", "ait"];
+    const dataPairObject = {
+      "Voler": "voler",
+      "Volent": "volent",
+      "Volait": "volait",
+    };
+    const filteredArray = engine.filterObjectByVerbTerminations(
+      arrayOfTerminations,
+      dataPairObject,
+    );
+
+    assertEquals(filteredArray, { "Voler": "voler" });
+  });
+
+  it("Should find one occurrence in word", () => {
+    const numberOfOccurrence = engine.findNumberOfOccurrenceInWord(
+      "tif",
+      "hâtif",
+    );
+    assertEquals(numberOfOccurrence, 1);
+  });
+
+  it("Should find two occurrence in word", () => {
+    const numberOfOccurrence = engine.findNumberOfOccurrenceInWord(
+      "tif",
+      "justificatif",
+    );
+    assertEquals(numberOfOccurrence, 2);
+  });
+
+  it("Should remove dash quotation", () => {
+    const newWord = engine.removeDashQuotation("ap-'Hair-o");
+
+    assertEquals(newWord, "ap'Hair-o");
+  });
+
+  it("Should capitalize first letter", () => {
+    const wordParts = "hâtif".split("tif");
+    const capitalizedWordParts = engine.capitalize(wordParts);
+
+    assertEquals(capitalizedWordParts, "Hâ");
+  });
+
+  it("Should replaceWordSegment", () => {
+    const wordParts1 = "hâtif".split("tif");
+    const replacingWord= 'Tif';
+    const newWord1 = engine.replaceWordSegment(wordParts1, replacingWord, false);
+
+    const wordParts2 = "apéritife".split("tif");
+    const newWord2 = engine.replaceWordSegment(wordParts2, replacingWord, true);
+
+    assertEquals(newWord1, "hâ'Tif");
+    assertEquals(newWord2, "apéri'Tif");
   });
 });
