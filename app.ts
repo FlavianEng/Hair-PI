34e7332303bcc9a881d15fc5bceb7226e6dbe2a1
Flavian Engevin <flavian.engevin@ynov.com>
2022-07-16 17:28:56 +0200

    Normalise responses

--- app.ts
+++ app.ts
@@ -2,11 +2,13 @@ import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./router.ts";
 import { errorHandler } from "./src/server/middlewares/errorHandler.ts";
 import { logger } from "./src/server/middlewares/logger.ts";
+import { successHandler } from "./src/server/middlewares/successHandler.ts";
 
 export const app = new Application();
 
 app.use(logger);
 app.use(errorHandler);
+app.use(successHandler); 
 
 const globalRouter = new Router();
 globalRouter.use("/api", router.routes());
---
+++ src/server/middlewares/successHandler.ts
@@ -0,0 +1,14 @@
+import {
+  Context,
+  Middleware,
+} from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+export const successHandler: Middleware = async (
+  ctx: Context,
+  next: () => Promise<unknown>,
+) => {
+
+  await next();
+  const body = ctx.response.body;
+  ctx.response.body = { status: ctx.response.status, data: body };
+};
